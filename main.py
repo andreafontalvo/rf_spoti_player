476afb0abc323aa8793ab716049127ae3dd323cd
Andrea Fontalvo Echavez <andreafontalvoechavez@gmail.com>
2022-02-13 20:55:26 +0100

    main

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
-#!/usr/bin/env python
-
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
+from time import sleep
+#from spotifyConnection import spotifyConnection
 
+# INIT libs instances
 reader = SimpleMFRC522()
-try:
-    id, text = reader.read()
-    print(id)
-    print(text)
-finally:
-    GPIO.cleanup() 
+# spotifyConnectionInstance = spotifyConnection()
+
+
+# INIT state variables
+currentAlbumID = None
+flagPlay = False
+
+# id, text = reader.read()
+
+print('Inicializando...')
+sleep(0.5)
+print('Esperando album RF')
+
+try: 
+  while True :
+
+    id, albumID = reader.read()
+
+    if albumID[0:8] == "spotify:" :
+      print(albumID)
+
+      # if albumID is currentAlbumID:
+      #   pass
+
+      # else:
+      if albumID != currentAlbumID :
+        print("PLAY nuevo album")
+        flagPlay = True
+        currentAlbumID = albumID
+
+      else :
+        if flagPlay:
+          print("PAUSE")
+          flagPlay = False
+
+        else : 
+          print("RESUME")
+          flagPlay = True
+    
+    sleep(2)
+
+except :
+  GPIO.cleanup()
